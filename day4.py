48dfeaf7fed77c78b73955c2d45bfe9e063db12d
Josefina Andreasson <josefina.andreasson@gmail.com>
2023-12-04 19:17:58 +0100

    put everything in functions

--- day4.py
+++ day4.py
@@ -1,32 +1,41 @@
 import numpy as np
 
-with open("inputs/day4", "r") as f:
-    lines = f.readlines()
+
+def parse_input():
+    with open("inputs/day4", "r") as f:
+        lines = f.readlines()
+
+    card_wins = []
+
+    for line in lines:
+        _, numbers = line.split(":")
+        winning_numbers, my_numbers = map(lambda x: set(x.split()), numbers.split("|"))
+        my_winning_numbers = winning_numbers.intersection(my_numbers)
+        card_wins.append(len(my_winning_numbers))
+
+    return card_wins
 
 
-# part 1
-def calc_points(wins):
-    if wins > 0:
-        return pow(2, wins - 1)
-    else:
-        return 0
+def solve_part1(card_wins):
+    def calc_points(wins):
+        if wins > 0:
+            return pow(2, wins - 1)
+        else:
+            return 0
+
+    return sum(map(calc_points, card_wins))
 
 
-card_wins = []
+def solve_part2(card_wins):
+    card_copies = np.ones(len(card_wins), dtype=int)
 
-for line in lines:
-    _, numbers = line.split(":")
-    winning_numbers, my_numbers = map(lambda x: set(x.split()), numbers.split("|"))
-    my_winning_numbers = winning_numbers.intersection(my_numbers)
-    card_wins.append(len(my_winning_numbers))
+    for i, wins in enumerate(card_wins):
+        card_copies[i + 1 : i + 1 + wins] += card_copies[i]
 
-print(sum(map(calc_points, card_wins)))
+    return sum(card_copies)
 
 
-# part 2
-card_copies = np.ones(len(lines), dtype=int)
-
-for i, wins in enumerate(card_wins):
-    card_copies[i + 1 : i + 1 + wins] += card_copies[i]
-
-print(sum(card_copies))
+if __name__ == "__main__":
+    card_wins = parse_input()
+    print(solve_part1(card_wins))
+    print(solve_part2(card_wins))
