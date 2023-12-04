c99fab51e56d7acc73b76e998013887fd5f4bec5
Josefina Andreasson <josefina.andreasson@gmail.com>
2023-12-04 08:57:27 +0100

    use sets

--- day4.py
+++ day4.py
@@ -1,6 +1,7 @@
 with open("inputs/day4", "r") as f:
     lines = f.readlines()
 
+
 # part 1
 def calc_points(wins):
     if wins > 0:
@@ -10,17 +11,12 @@ def calc_points(wins):
 
 
 card_wins = []
+
 for line in lines:
     _, numbers = line.split(":")
-    winning_numbers, my_numbers = map(lambda x: x.split(), numbers.split("|"))
-
-    matches = [
-        winning_number
-        for winning_number in winning_numbers
-        if winning_number in my_numbers
-    ]
-
-    card_wins.append(len(matches))
+    winning_numbers, my_numbers = map(lambda x: set(x.split()), numbers.split("|"))
+    my_winning_numbers = winning_numbers.intersection(my_numbers)
+    card_wins.append(len(my_winning_numbers))
 
 print(sum(map(calc_points, card_wins)))
 
@@ -28,11 +24,12 @@ print(sum(map(calc_points, card_wins)))
 # part 2
 def add_copies(cards, index, number_of_wins, number_of_copies):
     for i in range(index + 1, index + 1 + number_of_wins):
-        if i < len(lines):
+        if i < len(card_wins):
             cards[i] += number_of_copies
 
 
-cards = [1] * len(lines)
+cards = [1] * len(card_wins)
+
 for index, wins in enumerate(card_wins):
     add_copies(cards, index, wins, cards[index])
 
