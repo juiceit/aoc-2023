e9401c44b25c6970486adfa0dfc22e9327b29ccd
Josefina Andreasson <josefina.andreasson@gmail.com>
2023-12-05 21:13:08 +0100

    day 5 almost

---
+++ day5.py
@@ -0,0 +1,75 @@
+import re
+from tqdm import tqdm
+
+
+def parse_map_line(map_line):
+    return [int(number) for number in map_line.split()]
+
+
+def parse_map(map_string):
+    map_lines = map_string.splitlines()
+    return [parse_map_line(m) for m in map_lines]
+
+
+def parse_seeds(seeds_line):
+    seed_strings = seeds_line.split(":")[1].split()
+    return [int(s) for s in seed_strings]
+
+
+def parse_input():
+    with open("inputs/day5", "r") as f:
+        seeds_line = f.readline()
+        lines = f.read()
+
+    seeds = parse_seeds(seeds_line)
+    map_strings = re.split("\n.* map:\n", lines)
+    maps = [parse_map(m) for m in map_strings if m != "\n"]
+    return (seeds, maps)
+
+
+def find_mapped_index(index, maps):
+    for m in maps:
+        destination = m[0]
+        source = m[1]
+        range = m[2]
+        if index >= source and index < source + range:
+            diff = index - source
+            return destination + diff
+    return index
+
+
+def find_location(seed, maps):
+    index = seed
+    for m in maps:
+        index = find_mapped_index(index, m)
+    return index
+
+
+def solve_part1(seeds, maps):
+    locations = [find_location(seed, maps) for seed in seeds]
+    return sorted(locations)[0]
+
+
+def pair_seeds(seeds):
+    return [(seeds[i], seeds[i + 1]) for i in range(0, len(seeds), 2)]
+
+
+def find_locations(seed_pair, maps):
+    seeds = range(seed_pair[0], seed_pair[0] + seed_pair[1])
+    return (find_location(seed, maps) for seed in seeds)
+
+
+def solve_part2(seeds, maps):
+    seed_pairs = pair_seeds(seeds)
+    locations = [
+        v
+        for seed_pair in tqdm(seed_pairs)
+        for v in tqdm(find_locations(seed_pair, maps), position=1)
+    ]
+    return sorted(locations)[0]
+
+
+if __name__ == "__main__":
+    seeds, maps = parse_input()
+    print(solve_part1(seeds, maps))
+    print(solve_part2(seeds, maps))
